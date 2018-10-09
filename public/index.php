c809a4b9bbdaf9f444472217de67231e3150339b
alexander <mail@kladov.xyz>
2018-10-09 23:48:19 +0300

    #13 doctrine/dbal

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types = 1);
 
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -18,6 +20,38 @@ try {
     $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
     $containerBuilder = new \DI\ContainerBuilder();
     $containerBuilder->addDefinitions(CONFIG_DIR.'/database.php');
+    $containerBuilder->addDefinitions(
+        [
+            \Symfony\Component\Routing\Matcher\UrlMatcherInterface::class         => function (\DI\Container $container) {
+                $requestContext = new RequestContext('/');
+                $urlMatcher     = new UrlMatcher($container->get('routes'), $requestContext);
+
+                return $urlMatcher;
+            },
+            \Afw\Component\Controller\Resolver\ControllerResolverInterface::class => function (\DI\Container $container) {
+                return $container->get(\Afw\Component\Controller\Resolver\ControllerResolver::class);
+            },
+            \Doctrine\DBAL\Connection::class                                      => function (\DI\Container $container) {
+                $connections = $container->get('connections');
+                $connection  = $container->get('connection');
+
+                $currentConnection = $connections[$connection] ?? null;
+                if (null === $currentConnection) {
+                    throw new \RuntimeException('Database connection not found');
+                }
+
+                $config           = new \Doctrine\DBAL\Configuration();
+                $connectionParams = array(
+                    'url'         => $currentConnection['url'],
+                    'search_path' => 'afw.public',
+                );
+
+                $conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+                return $conn;
+            },
+        ]
+    );
     $containerBuilder->addDefinitions(CONFIG_DIR.'/services.php');
     $containerBuilder->useAnnotations(true);
 
